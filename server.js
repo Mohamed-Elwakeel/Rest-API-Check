9b25192925dc376a4647df370c6af5a94dce2249
Mohamed Elwakeel <mo7mdhassan7@gmail.com>
2023-06-06 11:54:28 +0300

    finished the get request

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ const User = require("./models/User");
 require("dotenv").config();
 const app = express();
 
-mongoose.connect(process.env.DB_CONNECTION_STRING).then(() => {
-  console.log("Connected to database");
+mongoose.connect(process.env.DATABASE_URL).then(() => {
+  console.log("connected successfully");
+});
+
+app.get("/users", (req, res) => {
+  User.find()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((error) => {
+      res.status(500).json({ message: error.message });
+    });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is listening on port ${process.env.PORT}`);
 });
